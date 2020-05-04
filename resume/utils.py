a89e3a26ebb3afcd46f602c9813df330b34d5357
gawema <gab.mannu@hotmail.it>
2020-05-04 10:41:38 +0200

    weasyprint test.

--- resume/utils.py
+++ resume/utils.py
@@ -2,13 +2,13 @@ from io import BytesIO
 from django.http import HttpResponse
 from django.template.loader import get_template
 
-from xhtml2pdf import pisa
+from weasyprint import HTML, CSS
+from weasyprint.fonts import FontConfiguration
 
 def render_to_pdf(template_src, context_dict={}):
     template = get_template(template_src)
     html  = template.render(context_dict)
-    result = BytesIO()
-    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
-    if not pdf.err:
-        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    pdf = HTML(string=html).write_pdf()
+    if pdf:
+        return pdf
     return None
