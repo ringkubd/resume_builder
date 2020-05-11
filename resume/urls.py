30853bc5360608f4470db8adc4a6d9f421559fcc
gawema <gab.mannu@hotmail.it>
2020-05-11 19:14:35 +0200

    New Design.

--- resume/urls.py
+++ resume/urls.py
@@ -9,5 +9,13 @@ app_name = 'resume'
 urlpatterns = [
     path('', views.home, name='home'),
     path('profile', views.profile, name='profile'),
+    path('profile/update_user', views.update_user, name='update_user'),
+    path('experience', views.experience, name='experience'),
+    path('experience/update_experience', views.update_experience, name='update_experience'),
+    path('experience/create_experience', views.create_experience, name='create_experience'),
+    path('education', views.education, name='education'),
+    path('education/update_education', views.update_education, name='update_education'),
+    path('education/create_education', views.create_education, name='create_education'),
+    path('settings', views.settings, name='settings'),
     path('test', views.test, name='test'),
 ]
--- resume/views.py
+++ resume/views.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,  get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+
 from .utils import render_to_pdf
 
 from .models import Experience, Education
+from user_profile.models import UserProfile
 
 
 def home(request):
@@ -13,12 +17,108 @@ def home(request):
 @login_required
 def profile(request):
 	user = request.user
+	profile = get_object_or_404(UserProfile, user=user)
 	context = {
-		'first_name': user.first_name,
-		'last_name': user.last_name,
+		'username': user.username,
 		'email': user.email,
+		'phone': profile.phone,
+		'profession': profile.profession,
+		'bio': profile.bio
 	}
 	return render(request, 'resume/profile.html', context)
+
+@login_required
+def experience(request):
+	user = request.user
+	experiences = Experience.objects.filter(user=request.user)
+	if experience:
+		context = {
+			'experiences': experiences,
+		}
+		return render(request, 'resume/experience.html', context)
+	return render(request, 'resume/experience.html')
+
+@login_required
+def education(request):
+	user = request.user
+	educations = Education.objects.filter(user=request.user)
+	if experience:
+		context = {
+			'educations': educations,
+		}
+		return render(request, 'resume/education.html', context)
+	return render(request, 'resume/education.html')
+
+@login_required
+def update_user(request):
+	user = request.user
+	user.username = request.POST['username']
+	user.email = request.POST['email']
+	user.save()
+	profile = get_object_or_404(UserProfile, user=user)
+	profile.phone = request.POST['phone']
+	profile.profession = request.POST['profession']
+	profile.bio = request.POST['bio']
+	profile.save()
+	return HttpResponseRedirect(reverse('resume:profile'))
+
+@login_required
+def create_experience(request):
+	experience = Experience()
+	experience.user = request.user
+	experience.company = request.POST['company']
+	experience.role = request.POST['role']
+	experience.startDate = request.POST['start']
+	experience.endDate = request.POST['end']
+	experience.description = request.POST['description']
+	experience.save()
+	return HttpResponseRedirect(reverse('resume:experience'))
+
+@login_required
+def update_experience(request):
+	id = request.POST['id']
+	experience =get_object_or_404(Experience, id=id)
+	experience.company = request.POST['company']
+	experience.role = request.POST['role']
+	# experience.startDate = request.POST['start']
+	# experience.endDate = request.POST['end']
+	experience.description = request.POST['description']
+	experience.save()
+	return HttpResponseRedirect(reverse('resume:experience'))
+
+@login_required
+def create_education(request):
+	education = Education()
+	education.user = request.user
+	education.school = request.POST['school']
+	education.degree = request.POST['degree']
+	education.startDate = request.POST['start']
+	education.endDate = request.POST['end']
+	education.description = request.POST['description']
+	education.save()
+	return HttpResponseRedirect(reverse('resume:education'))
+
+@login_required
+def update_education(request):
+	id = request.POST['id']
+	education =get_object_or_404(Education, id=id)
+	education.school = request.POST['school']
+	education.degree = request.POST['degree']
+	# education.startDate = request.POST['start']
+	# education.endDate = request.POST['end']
+	education.description = request.POST['description']
+	education.save()
+	return HttpResponseRedirect(reverse('resume:education'))
+
+@login_required
+def settings(request):
+	user = request.user
+	profile = get_object_or_404(UserProfile, user=user)
+	context = {
+		'username': user.username,
+		'email': user.email,
+	}
+	return render(request, 'resume/settings.html', context)
 
 
 @login_required
